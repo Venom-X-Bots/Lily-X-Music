7bb15279ff0ee3fdf2183a3e38e122b1b8ab8451
ARYAN <154561740+Venom-PRIVATE-BOTS@users.noreply.github.com>
2023-12-23 03:30:25 +0530

    Update __main__.py

--- LilyXMusic/__main__.py
+++ LilyXMusic/__main__.py
@@ -38,9 +38,9 @@ async def init():
         importlib.import_module("LilyXMusic.plugins" + all_module)
     LOGGER("LilyXMusic.plugins").info("Successfully Imported Modules...")
     await userbot.start()
-    await Anony.start()
+    await Lilyy.start()
     try:
-        await Anony.stream_call("https://telegra.ph/file/4b622cc566a681694728f.mp4")
+        await Lilyy.stream_call("https://telegra.ph/file/4b622cc566a681694728f.mp4")
     except NoActiveGroupCall:
         LOGGER("LilyXMusic").error(
             "Please turn on the videochat of your log group\channel.\n\nStopping Bot..."
@@ -48,7 +48,7 @@ async def init():
         exit()
     except:
         pass
-    await Anony.decorators()
+    await Lilyy.decorators()
     LOGGER("LilyXMusic").info(
         "\x41\x6e\x6f\x6e\x58\x20\x4d\x75\x73\x69\x63\x20\x42\x6f\x74\x20\x53\x74\x61\x72\x74\x65\x64\x20\x53\x75\x63\x63\x65\x73\x73\x66\x75\x6c\x6c\x79\x2e\n\n\x44\x6f\x6e'\x74\x20\x66\x6f\x72\x67\x65\x74\x20\x74\x6f\x20\x76\x69\x73\x69\x74\x20\x40\x46\x61\x6c\x6c\x65\x6e\x41\x73\x73\x6f\x63\x69\x61\x74\x69\x6f\x6e"
     )
