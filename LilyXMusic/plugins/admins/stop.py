c66ae7a14c5d075c8a80d2d569800014c6956c2b
ARYAN <154561740+Venom-PRIVATE-BOTS@users.noreply.github.com>
2023-12-23 05:22:29 +0530

    Update stop.py

--- LilyXMusic/plugins/admins/stop.py
+++ LilyXMusic/plugins/admins/stop.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import set_loop
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from LilyXMusic import app
+from LilyXMusic.core.call import Anony
+from LilyXMusic.utils.database import set_loop
+from LilyXMusic.utils.decorators import AdminRightsCheck
+from LilyXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
