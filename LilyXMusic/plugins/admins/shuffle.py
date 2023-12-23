999e0593d54d4bc5f3c75e5972186b6c9360f6dc
ARYAN <154561740+Venom-PRIVATE-BOTS@users.noreply.github.com>
2023-12-23 05:33:15 +0530

    Update shuffle.py

--- LilyXMusic/plugins/admins/shuffle.py
+++ LilyXMusic/plugins/admins/shuffle.py
@@ -3,10 +3,10 @@ import random
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.misc import db
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from LilyXMusic import app
+from LilyXMusic.misc import db
+from LilyXMusic.utils.decorators import AdminRightsCheck
+from LilyXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
