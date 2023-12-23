c51469f7e18cc5dde442f7c729f615207110b311
ARYAN <154561740+Venom-PRIVATE-BOTS@users.noreply.github.com>
2023-12-23 05:35:39 +0530

    Update pause.py

--- LilyXMusic/plugins/admins/pause.py
+++ LilyXMusic/plugins/admins/pause.py
@@ -1,11 +1,11 @@
 from pyrogram import filters
 from pyrogram.types import Message
 
-from AnonXMusic import app
-from AnonXMusic.core.call import Anony
-from AnonXMusic.utils.database import is_music_playing, music_off
-from AnonXMusic.utils.decorators import AdminRightsCheck
-from AnonXMusic.utils.inline import close_markup
+from LilyXMusic import app
+from LilyXMusic.core.call import Lilyy
+from LilyXMusic.utils.database import is_music_playing, music_off
+from LilyXMusic.utils.decorators import AdminRightsCheck
+from LilyXMusic.utils.inline import close_markup
 from config import BANNED_USERS
 
 
