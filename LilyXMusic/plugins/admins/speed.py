d0352bbdb849f554761f76c79091580c794073f0
ARYAN <154561740+Venom-PRIVATE-BOTS@users.noreply.github.com>
2023-12-23 05:30:29 +0530

    Update speed.py

--- LilyXMusic/plugins/admins/speed.py
+++ LilyXMusic/plugins/admins/speed.py
@@ -2,7 +2,7 @@ from pyrogram import filters
 from pyrogram.types import Message
 
 from LilyXMusic import app
-from LilyXMusic.core.call import Anony
+from LilyXMusic.core.call import Lilyy
 from LilyXMusic.misc import SUDOERS, db
 from LilyXMusic.utils import AdminRightsCheck
 from LilyXMusic.utils.database import is_active_chat, is_nonadmin_chat
